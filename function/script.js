6b48b65a1c248d900224ed9aac1fad9335db94f4
Mann Patel <169169782+mannpatel08@users.noreply.github.com>
2025-08-12 19:13:12 +0530

    function

---
+++ function/script.js
@@ -0,0 +1,15 @@
+// function sum(a,b){
+//     return a+b;
+// }
+
+// let addition = sum(10,9);
+
+// console.log(`Addition of Two Sum is : ${addition}`);
+
+function changeText(){
+    document.getElementById("title").innerHTML = "Welcome World";
+}
+
+function changeColor(color){
+    document.body.style.backgroundColor = color;
+}
