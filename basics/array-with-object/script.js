2bef5c6186ba754c2ed009f08e75d7861794af78
Mann Patel <169169782+mannpatel08@users.noreply.github.com>
2025-08-21 19:03:22 +0530

    update

---
+++ basics/array-with-object/script.js
@@ -0,0 +1,38 @@
+// let ganpatiGroup = {
+//     groupName : "Malhari Group",
+//     ESTD : 2014,
+//     location : "SVNIT, Piplod"
+// };
+
+// console.log(`Group Name : ${ganpatiGroup.groupName}`);
+// console.log(`ESTD : ${ganpatiGroup.ESTD}`);
+// console.log(`Group Location : ${ganpatiGroup.location}`);
+
+
+let studant = [
+    {
+        Name : "Mann Patel",
+        Age : 20,
+        College : "SDJ International College"
+    },
+    {
+        Name : "Aman Jaiswal",
+        Age : 22,
+        College : "Nevar Attended College"
+    },
+    {
+        Name : "Hardik Bhavsar",
+        Age : 19,
+        College : "Sasma Public College"
+    },
+    {
+        Name : "Tanishk Shah",
+        Age : 21,
+        College : "Drop Out"
+    }
+]
+
+console.log(`Studant Name : ${studant[0].Name}, Age : ${studant[0].Age}, College Name : ${studant[0].College}`)
+console.log(`Studant Name : ${studant[1].Name}, Age : ${studant[1].Age}, College Name : ${studant[1].College}`)
+console.log(`Studant Name : ${studant[2].Name}, Age : ${studant[2].Age}, College Name : ${studant[2].College}`)
+console.log(`Studant Name : ${studant[3].Name}, Age : ${studant[3].Age}, College Name : ${studant[3].College}`)
---
+++ basics/array-with-object/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+let ganpatiGroup = {
+//     groupName : "Malhari Group",
+//     ESTD : 2014,
+//     location : "SVNIT, Piplod"
+// };
+
+// console.log(`Group Name : ${ganpatiGroup.groupName}`);
+// console.log(`ESTD : ${ganpatiGroup.ESTD}`);
+// console.log(`Group Location : ${ganpatiGroup.location}`);
